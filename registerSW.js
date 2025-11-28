bbe692f67914383cd17b30b9db08f4274fca4f6c
Ivan Filho <ivan.silva@dcx.ufpb.br>
2025-11-28 18:05:23 -0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/CAMINHOS-DO-PENSAMENTO-COMPUTACIONAL--PROJETO-TCC/sw.js', { scope: '/CAMINHOS-DO-PENSAMENTO-COMPUTACIONAL--PROJETO-TCC/' })})}
